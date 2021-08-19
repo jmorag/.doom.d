047d6d974a031383f08a4c22e39553b91bc8617d
Joseph Morag <jm@josephmorag.com>
2021-08-19 11:08:45 -0400

    Notmuch updates

--- modules/email/notmuch/config.el
+++ modules/email/notmuch/config.el
@@ -1,23 +1,34 @@
 ;;; email/notmuch/config.el -*- lexical-binding: t; -*-
 
 (use-package! notmuch
+  :custom (notmuch-search-oldest-first nil)
   :init
   (after! org
     (add-to-list 'org-modules 'ol-notmuch))
   :config
   (set-company-backend! 'notmuch-message-mode
-    'notmuch-company '(company-ispell company-yasnippet))  :config
-  (setq notmuch-search-oldest-first nil
-        send-mail-function 'sendmail-sent-it
-        sendmail-program "msmtp"
-        message-send-mail-function 'message-send-mail-with-sendmail
-        notmuch-poll-script "/home/joseph/Mail/checkmail.sh"
-        notmuch-show-logo nil
-        mm-text-html-renderer 'shr
-        notmuch-multipart/alternative-discouraged '()
-        notmuch-always-prompt-for-sender t)
+    'notmuch-company '(company-ispell company-yasnippet))
+  (setq! send-mail-function 'sendmail-sent-it
+         sendmail-program "msmtp"
+         message-send-mail-function 'message-send-mail-with-sendmail
+         notmuch-poll-script "/home/joseph/Mail/checkmail.sh"
+         notmuch-show-logo nil
+         mm-text-html-renderer 'shr
+         notmuch-multipart/alternative-discouraged '()
+         notmuch-always-prompt-for-sender t
+         notmuch-saved-searches
+         '((:name "inbox" :query "tag:inbox date:30d..now" :key "i")
+           (:name "unread" :query "tag:unread" :key "u")
+           (:name "flagged" :query "tag:flagged" :key "f")
+           (:name "sent" :query "tag:sent" :key "t")
+           (:name "drafts" :query "tag:draft" :key "d")
+           (:name "all mail" :query "*" :key "a")
+           (:name "digest" :query "tag:digest")
+           (:name "lists" :query "tag:lists")
+           (:name "recurse" :query "to:@lists.community.recurse.com")))
   (map!
    "C-c m" #'notmuch
+   ;; (:when (featurep! :completion helm) "C-c m" #'helm-notmuch)
    :map notmuch-search-mode-map
    "u" #'notmuch-search-toggle-unread
    "f" #'notmuch-search-toggle-flagged
