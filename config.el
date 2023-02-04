1fe9d37dec3e3df193bc72363188f0534248ec1d
Joseph Morag <jm@josephmorag.com>
2023-02-04 16:03:47 -0500

    Fix hydra

--- config.el
+++ config.el
@@ -165,6 +165,46 @@
   "C-j" #'next-line-or-history-element
   "C-k" #'previous-line-or-history-element)
 
+(after! hydra
+  (defhydra +hydra/windower (:hint nil)
+    "
+^Move^                      ^Swap^
+^^^^^^^^------------------------------------------------
+_h_: move border left       _H_: swap border left
+_j_: move border up         _J_: swap border above
+_k_: move border down       _K_: swap border below
+_l_: move border right      _L_: swap border right
+                          _t_: toggle vertical or horizontal split
+"
+    ("h" windower-move-border-left)
+    ("j" windower-move-border-below)
+    ("k" windower-move-border-above)
+    ("l" windower-move-border-right)
+    ("<left>" windower-move-border-left)
+    ("<down>" windower-move-border-below)
+    ("<up>" windower-move-border-above)
+    ("<right>" windower-move-border-right)
+    ("H" windower-swap-left)
+    ("J" windower-swap-below)
+    ("K" windower-swap-above)
+    ("L" windower-swap-right)
+    ("<S-left>" windower-swap-left)
+    ("<S-down>" windower-swap-below)
+    ("<S-up>" windower-swap-above)
+    ("<S-right>" windower-swap-right)
+    ("t" windower-toggle-split)
+    ("s" windower-swap :exit t)
+    ("q" nil "cancel" :color blue)
+    ("<return>" nil nil))
+  (defhydra +hydra/smerge ()
+  "A hydra for smerge"
+  ("j" smerge-next "next conflict")
+  ("k" smerge-prev "previous conflict")
+  ("u" smerge-keep-upper "keep upper conflict")
+  ("l" smerge-keep-lower "keep lower conflict")
+  ("b" smerge-keep-all "keep both")
+  ("q" nil "cancel" :color blue)))
+
 (map!
  "C-z" nil
  "C-;" nil
